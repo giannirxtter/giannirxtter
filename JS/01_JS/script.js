3312506ca3d6ab27c954fc17a16127e8119b012e
giannirxtter <gianlucaritter18.04@icloud.com>
2024-03-14 09:04:01 +0100

    x

---
+++ JS/01_JS/script.js
@@ -0,0 +1,36 @@
+console.log("Hi");
+console.log("Wie gehts");
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+//Berechne folgendes und gib das ergebnis ueber console.log aus
+// a + b + c
+// (a+b) /c
+// c-a + b
+//a + b + c * 3
+// (a+b+c)/ 4
+
+// Aendere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c - 3
+// gib die neuen Werte der Variablen aus
+
+let result1 = a + b + c;
+console.log(result1);
+let result2 = ( a + b)/ c;
+console.log(result2);
+let result3 = c - a + b;
+console.log(result3);
+
+a = 3;
+b = a +3;
+c = c - 3;
+
+let result4 = a + b + c;
+console.log(result4);
+let result5 = (a+b) / c;
+console.log(result5);
+let result6 = c - a + b;
+console.log(result6);
---
+++ JS/09_JS/script.js
@@ -0,0 +1,25 @@
+for (let i = 0; i < 4; i ++){
+    if(i % 2 == 0){
+        console.log("XXXX");
+    }
+    else{
+        console.log("0000");
+    }
+}
+
+let counter = 0
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        counter = counter + i
+    }
+    console.log(counter);
+}
+
+for (let i = 0; i < 5; i++) {
+    if (i % 2 == 0) {
+        console.log("x");
+    }
+    else {
+        console.log("xxxxxx");
+    }
+}
